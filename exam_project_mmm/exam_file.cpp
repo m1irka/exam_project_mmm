833a38ffb61199f3140fe35983a93320dc247e68
Myroslava Sthembulak <miroslavastembulak@gmail.com>
2025-05-25 17:50:56 +0300

    добавлений функции

--- exam_project_mmm/exam_file.cpp
+++ exam_project_mmm/exam_file.cpp
@@ -1,87 +1,160 @@
 #include <iostream>
-#include "Function.h"
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-int main()
-{
-    int playChoice, compChoice, rounds;
+void printHeader() {
+    cout << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << "|                Rock, Scissors, Paper Game            |" << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << endl;
+}
+
+void printRules() {
+    cout << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << "| 1. Rock (1)                                          |" << endl;
+    cout << "| 2. Scissors (2)                                      |" << endl;
+    cout << "| 3. Paper (3)                                         |" << endl;
+    cout << "--------------------------------------------------------" << endl;
+    cout << endl;
+}
+
+void displayMessage() {
+    const string messages[] = {
+        " Be careful! A stone is falling on you!",
+        " I can already taste my victory...)",
+        " I think you're giving up, man!",
+        " Just don't be a loser, bro...",
+        " People surprise me a lot...",
+        " Do you want to compete with the computer?",
+        " Beware with scissors. Don't cut off your finger.",
+        " Do you like paper?",
+        " Beats me! Who will win?"
+    };
+    int random_index = rand() % 9;
+    cout << messages[random_index] << endl;
+}
+
+int main() {
+    srand(time(0));
+    int playerChoice, compChoice, rounds;
     int wins = 0, losses = 0, draws = 0;
 
-    cout << " Game: Rock, Scissors, Paper!" << endl
-        << "\033[36m Rules: Choose one of the objects and see what your opponent (the computer) has chosen\033[0m" << endl
-        << "\033[36m Remember, paper covers rock, rock breaks scissors, and scissors cut paper.\033[0m" << endl;
+    printHeader();
+    cout << "\033[36m Rules: Choose one of the objects and see what your opponent (the computer) has chosen\033[0m" << endl;
+    cout << "\033[36m Remember, paper covers rock, rock breaks scissors, and scissors cut paper.\033[0m" << endl;
 
-    cout << "Enter number of rounds: ";
+    cout << " Enter number of rounds: ";
     cin >> rounds;
 
-    for (int i = 0; i < rounds; i++)
-    {
-        cout << endl << "\033[33mRound " << i + 1 << ": Choose rock (1), scissors (2), paper (3): \033[0m";
-        cin >> playChoice;
+    for (int round = 1; round <= rounds; round++) {
+        cout << "\nRound " << round << " of " << rounds << endl;
+        displayMessage();
+        printRules();
+
+       
+        bool validInput = false;
+        do {
+            cout << "Enter your choice (1-3): ";
+            cin >> playerChoice;
+
+            if (playerChoice >= 1 && playerChoice <= 3) {
+                validInput = true;
+            }
+            else {
+                cout << "\033[31mError: Invalid choice!\033[0m ";
+                int phraseError = rand() % 5 + 1;
+                switch (phraseError) {
+                case 1: cout << "Dummy man...\n"; break;
+                case 2: cout << "What are you trying to achieve?\n"; break;
+                case 3: cout << "Who are the rules for?\n"; break;
+                case 4: cout << "You're not silly, are you?\n"; break;
+                case 5: cout << "The mistake is behind you)\n"; break;
+                }
+            }
+        } while (!validInput);
 
         compChoice = rand() % 3 + 1;
 
-        cout << "------------------------------------------------------------------------------------------------------------------------" << endl;
-        cout << "\033[33mYou: \033[0m";
-        if (playChoice == 1)
-        {
-            cout << "rock" << endl;
+     
+        cout << "-----------------------------------------------------------------------" << endl;
+        cout << "       \033[33mYou: \033[0m";
+        switch (playerChoice) {
+        case 1: cout << "rock     "; break;
+        case 2: cout << "scissors "; break;
+        case 3: cout << "paper    "; break;
         }
-        else if (playChoice == 2)
-        {
-            cout << "scissors" << endl;
-        }
-        else if (playChoice == 3)
-        {
-            cout << "paper" << endl;
-        }
-        else
-        {
-            cout << "\033[31mError: Invalid choice!\033[0m" << endl;
-            i--;
-            continue;
+        cout << "| \033[35mComputer: \033[0m";
+        switch (compChoice) {
+        case 1: cout << "rock\n"; break;
+        case 2: cout << "scissors\n"; break;
+        case 3: cout << "paper\n"; break;
         }
 
-        cout << "\033[35mThe computer: \033[0m";
-        if (compChoice == 1)
-        {
-            cout << "rock" << endl;
-        }
-        else if (compChoice == 2)
-        {
-            cout << "scissors" << endl;
-        }
-        else
-        {
-            cout << "paper" << endl;
-        }
-
-
-        if (playChoice == compChoice)
-        {
-            cout << "\033[34mDraw\033[0m" << endl;
+    
+        if (playerChoice == compChoice) {
+            cout << "\033[34m Draw!\033[0m" << endl;
             draws++;
         }
-        else if ((playChoice == 1 && compChoice == 2) || (playChoice == 2 && compChoice == 3) || (playChoice == 3 && compChoice == 1))
-        {
-            cout << "\033[32mVictory!\033[0m" << endl;
+        else if ((playerChoice == 1 && compChoice == 2) ||
+            (playerChoice == 2 && compChoice == 3) ||
+            (playerChoice == 3 && compChoice == 1)) {
+            cout << "\033[32m Victory!\033[0m" << endl;
             wins++;
         }
-        else
-        {
-            cout << "\033[31mDefeat...\033[0m" << endl;
+        else {
+            cout << "\033[31m Defeat...\033[0m" << endl;
             losses++;
         }
 
-        cout << "------------------------------------------------------------------------------------------------------------------------" << endl;
+        cout << "[Wins: " << wins << " | Losses: " << losses << " | Draws: " << draws << "]" << endl;
+
+
+        if (playerChoice == compChoice) {
+            int phraseDraw = rand() % 5 + 1;
+            switch (phraseDraw) {
+            case 1: cout << " Why is this man equal to me?\n"; break;
+            case 2: cout << " I like it better when you lose.\n"; break;
+            case 3: cout << " You're smarter than I thought.\n"; break;
+            case 4: cout << " Weak to beat me?\n"; break;
+            case 5: cout << " Not bad.\n"; break;
+            }
+        }
+        else if (playerChoice > compChoice || (playerChoice == 1 && compChoice == 3)) {
+            int phraseWin = rand() % 5 + 1;
+            switch (phraseWin) {
+            case 1: cout << " I'll get the win back!\n"; break;
+            case 2: cout << " You're lucky.\n"; break;
+            case 3: cout << " That's not fair.\n"; break;
+            case 4: cout << " You're being nice.\n"; break;
+            case 5: cout << " Are you smart?\n"; break;
+            }
+        }
+        else {
+            int phraseLose = rand() % 6 + 1;
+            switch (phraseLose) {
+            case 1: cout << " Ha-ha...\n"; break;
+            case 2: cout << " As always!\n"; break;
+            case 3: cout << " Machines win!\n"; break;
+            case 4: cout << " I love beating you.\n"; break;
+            case 5: cout << " Don't be upset.\n"; break;
+            case 6: cout << " How do you feel?\n"; break;
+            }
+        }
+    }
+    cout << "\n\033[36m Game Over!\033[0m" << endl;
+    cout << "\033[32m Wins: " << wins << "\033[0m | ";
+    cout << "\033[31mLosses: " << losses << "\033[0m | ";
+    cout << "\033[34mDraws: " << draws << "\033[0m" << endl;
+
+    if (wins + losses + draws > 0) {
+        cout << "\n\033[35m Thanks for playing!\033[0m" << endl;
+    }
+    else {
+        cout << "Did you even play?" << endl;
     }
 
-
-    cout << "\n\033[36mGame Over!\033[0m" << endl;
-    cout << "\033[32mWins: " << wins << "\033[0m | "
-        << "\033[31mLosses: " << losses << "\033[0m | "
-        << "\033[34mDraws: " << draws << "\033[0m" << endl;
-
-    cout << "\n\033[35mThanks for playing!\033[0m" << endl;
     return 0;
 }
