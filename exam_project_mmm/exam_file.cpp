24354cecd43f4ceaa31c02c2fa0e0c27ce0cbcc8
Myroslava Sthembulak <miroslavastembulak@gmail.com>
2025-05-28 19:42:47 +0300

    Больше Фунций

--- exam_project_mmm/exam_file.cpp
+++ exam_project_mmm/exam_file.cpp
@@ -22,7 +22,7 @@ void printRules() {
 }
 
 void displayMessage() {
-    const string messages[] = {
+    const char* messages[] = {
         " Be careful! A stone is falling on you!",
         " I can already taste my victory...)",
         " I think you're giving up, man!",
@@ -36,6 +36,56 @@ void displayMessage() {
     int random_index = rand() % 9;
     cout << messages[random_index] << endl;
 }
+
+
+void PrintPhraseDraw() 
+{
+    int phraseDraw = rand() % 5 + 1;
+    switch (phraseDraw) {
+    case 1: cout << " Why is this man equal to me?\n"; break;
+    case 2: cout << " I like it better when you lose.\n"; break;
+    case 3: cout << " You're smarter than I thought.\n"; break;
+    case 4: cout << " Weak to beat me?\n"; break;
+    case 5: cout << " Not bad.\n"; break;
+    }
+}
+
+void PrintPhraseWin()
+{
+    int phraseWin = rand() % 5 + 1;
+    switch (phraseWin) {
+    case 1: cout << " I'll get the win back!\n"; break;
+    case 2: cout << " You're lucky.\n"; break;
+    case 3: cout << " That's not fair.\n"; break;
+    case 4: cout << " You're being nice.\n"; break;
+    case 5: cout << " Are you smart?\n"; break;
+    }
+}
+
+void PrintPhraseLose()
+{
+    int phraseLose = rand() % 6 + 1;
+    switch (phraseLose) {
+    case 1: cout << " Ha-ha...\n"; break;
+    case 2: cout << " As always!\n"; break;
+    case 3: cout << " Machines win!\n"; break;
+    case 4: cout << " I love beating you.\n"; break;
+    case 5: cout << " Don't be upset.\n"; break;
+    case 6: cout << " How do you feel?\n"; break;
+    }
+}
+
+void printRandomErrorMessage() {
+    int phraseError = rand() % 5 + 1;
+    switch (phraseError) {
+    case 1: std::cout << "Dummy man...\n"; break;
+    case 2: std::cout << "What are you trying to achieve?\n"; break;
+    case 3: std::cout << "Who are the rules for?\n"; break;
+    case 4: std::cout << "You're not silly, are you?\n"; break;
+    case 5: std::cout << "The mistake is behind you)\n"; break;
+    }
+}
+
 
 int main() {
     srand(time(0));
@@ -60,21 +110,20 @@ int main() {
             cout << "Enter your choice (1-3): ";
             cin >> playerChoice;
 
-            if (playerChoice >= 1 && playerChoice <= 3) {
+            if (playerChoice >= 1 && playerChoice <= 3) 
+            {
                 validInput = true;
             }
-            else {
+            else 
+            {
+                
                 cout << "\033[31mError: Invalid choice!\033[0m ";
-                int phraseError = rand() % 5 + 1;
-                switch (phraseError) {
-                case 1: cout << "Dummy man...\n"; break;
-                case 2: cout << "What are you trying to achieve?\n"; break;
-                case 3: cout << "Who are the rules for?\n"; break;
-                case 4: cout << "You're not silly, are you?\n"; break;
-                case 5: cout << "The mistake is behind you)\n"; break;
+                printRandomErrorMessage();
+                
                 }
             }
-        } while (!validInput);
+        
+        while (!validInput);
 
         compChoice = rand() % 3 + 1;
 
@@ -112,36 +161,16 @@ int main() {
         cout << "[Wins: " << wins << " | Losses: " << losses << " | Draws: " << draws << "]" << endl;
 
 
-        if (playerChoice == compChoice) {
-            int phraseDraw = rand() % 5 + 1;
-            switch (phraseDraw) {
-            case 1: cout << " Why is this man equal to me?\n"; break;
-            case 2: cout << " I like it better when you lose.\n"; break;
-            case 3: cout << " You're smarter than I thought.\n"; break;
-            case 4: cout << " Weak to beat me?\n"; break;
-            case 5: cout << " Not bad.\n"; break;
-            }
+        if (playerChoice == compChoice) 
+        {
+            PrintPhraseDraw();
         }
-        else if (playerChoice > compChoice || (playerChoice == 1 && compChoice == 3)) {
-            int phraseWin = rand() % 5 + 1;
-            switch (phraseWin) {
-            case 1: cout << " I'll get the win back!\n"; break;
-            case 2: cout << " You're lucky.\n"; break;
-            case 3: cout << " That's not fair.\n"; break;
-            case 4: cout << " You're being nice.\n"; break;
-            case 5: cout << " Are you smart?\n"; break;
-            }
+        else if (playerChoice > compChoice || (playerChoice == 1 && compChoice == 3)) 
+        {
+            PrintPhraseWin();
         }
         else {
-            int phraseLose = rand() % 6 + 1;
-            switch (phraseLose) {
-            case 1: cout << " Ha-ha...\n"; break;
-            case 2: cout << " As always!\n"; break;
-            case 3: cout << " Machines win!\n"; break;
-            case 4: cout << " I love beating you.\n"; break;
-            case 5: cout << " Don't be upset.\n"; break;
-            case 6: cout << " How do you feel?\n"; break;
-            }
+			PrintPhraseLose();
         }
     }
     cout << "\n\033[36m Game Over!\033[0m" << endl;
@@ -150,7 +179,7 @@ int main() {
     cout << "\033[34mDraws: " << draws << "\033[0m" << endl;
 
     if (wins + losses + draws > 0) {
-        cout << "\n\033[35m Thanks for playing!\033[0m" << endl;
+        cout << "\n\033[35m Thanks for playing! ^_^ \033[0m" << endl;
     }
     else {
         cout << "Did you even play?" << endl;
